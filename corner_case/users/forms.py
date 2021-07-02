005443c683689e78d809cacdd23a99935465d6a3
supto09 <supto09apee@gmail.com>
2021-07-02 22:16:45 +0600

    user model added to admin

--- corner_case/users/forms.py
+++
@@ -1,3 +0,0 @@
-from django.contrib.auth import forms as admin_forms
-from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
--- corner_case/users/models.py
+++ corner_case/users/models.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 
 
 class UserManager(BaseUserManager):
