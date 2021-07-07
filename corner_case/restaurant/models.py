5c6f4ff42a028bd8915cfe7187893acebb500c31
supto09 <supto09apee@gmail.com>
2021-07-07 14:37:49 +0600

    winner selection api added

--- corner_case/restaurant/models.py
+++ corner_case/restaurant/models.py
@@ -12,6 +12,8 @@ class Menu(models.Model):
     restaurant = models.ForeignKey(Restaurant, blank=False, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, blank=False)
     date = models.DateField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         constraints = [
@@ -19,4 +21,4 @@ class Menu(models.Model):
         ]
 
     def __str__(self):
-        return self.name
+        return str(self.id) + "=>" + self.name
